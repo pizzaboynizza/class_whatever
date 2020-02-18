643515cf26d6e47a16f3ac9aa1809d1a1164a6fe
NickGallo <nickgallo41@gmail.com>
2020-02-18 15:45:27 -0800

    lab01 complete - upload to git

---
+++ code/ngallo/lab01-turtle.py
@@ -0,0 +1,25 @@
+from turtle import *
+
+backward(60)
+forward(100)
+left(90)
+forward(100)
+backward(200)
+forward(100)
+right(90)
+forward(35)
+right(90)
+i = 0
+while i < 100:
+    forward(2)
+    left(360/100)
+    i = i + 1
+left(90)
+backward(200)
+left(25)
+backward(100)
+forward(100)
+right(50)
+backward(100)
+
+done()
