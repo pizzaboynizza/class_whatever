a398bc1c276cfa91f5b9b4a34e5ec5097a3651b8
Justin Nizza <pizzaboynizza@Richards-iPhone.localdomain>
2020-02-19 15:35:42 -0800

    stupid sexy flanders

---
+++ code/Justin/lab11.py
@@ -0,0 +1,6 @@
+user_operation = input("What is the operation you'd like to perform?")
+
+user_first_number = input("What is the first number?")
+
+user_second_number = input("What is the second number?")
+
