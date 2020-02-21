c079000ef882cd6bdfcc5effa3a28fca02b3d209
JoHannah Joy <jojojoy2012@gmail.com>
2020-02-21 13:42:57 -0800

    complete practice 1 problem 3 number within 10 and 100 inclusive

---
+++ code/johannah/problem3practice1.py
@@ -0,0 +1,8 @@
+user_input = []
+def near_100(user_input):
+  user_input = float(input("Enter a number to dtermine if it is within 10 and 100: "))
+  if user_input >= 10 and user_input <= 100:
+    print("True")
+  else:
+    print("False")
+near_100(user_input)
