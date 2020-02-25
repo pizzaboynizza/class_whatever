cebfcc6eefc7ab3783602a74df7e101dd1de143a
TinaShreves <tina.l.shreves@gmail.com>
2020-02-25 14:20:17 -0800

    jackelope lab v1 complete. upload to repo

--- code/nbarker/group-nik-nik-michael-tina.py
+++ code/nbarker/group-nik-nik-michael-tina.py
@@ -7,21 +7,26 @@ jackelope_age = [0,0]
 
 year = 0
 age = 0
-while len(jackelope_age) <= 15:
-    killed_jackelopes = []
+
+while len(jackelope_age) <= 1000:
+  
     num_births = 0
+
     for index in range(len(jackelope_age)):
         jackelope_age[index] += 1
-        if jackelope_age[index] >= 10:
-            killed_jackelopes.append(index)
-        if 4 < jackelope_age[index] < 9 :
+        if 3 < jackelope_age[index] < 9 :
             num_births += 1
-    num_births = num_births//2
-    for death in killed_jackelopes:
-        jackelope_age.pop(death)
+
+    # num_births = num_births//2
+
+    while jackelope_age.count(10) > 0:
+        jackelope_age.remove(10)
+
     for i in range(num_births):
         jackelope_age.append(0)
+    
     print(jackelope_age)
+    
     year += 1
 else: 
     print("meow")
