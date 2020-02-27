186d5b77676e7937b7a41d2bba91dfa5eab12532
JoHannah Joy <jojojoy2012@gmail.com>
2020-02-27 15:48:27 -0800

    begin labs 21 and 22 and complete lab 19 blackjack

--- code/johannah/lab19-blackjack.py
+++ code/johannah/lab19-blackjack.py
@@ -12,7 +12,24 @@ Use the following rules to determine the advice:
 -Print out the current total point value and the advice.
 '''
 
-three_cards = input("Enter three card choices (A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, or K): ")
+three_cards = []
+card_one = input("Enter first card (A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, or K): ")
+card_two = input("Enter second card (A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, or K): ")
+card_three = input("Enter third card (A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, or K): ")
+# append card_one, card_two, & card_three into a list
+three_cards.append(card_one)
+three_cards.append(card_two)
+three_cards.append(card_three)
+print(three_cards)
+
+
+# def my_func():
+#     if card_one or card_two or card_three == 'A':
+#         return 1
+#     #elif card == 'J' or card == 'Q' or card == 'K':
+#     elif card_one or card_two or card_three == 'J' or 'Q' or 'K':
+#         return 10
+#     #x = int(card_one + card_two + card_three)
 conversion = {
     "A": 1,
     "2": 2,
@@ -28,9 +45,24 @@ conversion = {
     "Q": 10,
     "K": 10,
 }
+
+
 #add 3 numbers from user input
+sum_three = 0
+for i in range(len(three_cards)):
+    sum_three = sum_three + conversion[three_cards[i]]
+print(sum_three)
+
 #if sum < 17, advise to "Hit"
+if sum_three < 17:
+    print(f'{sum_three}, I advise you to \'Hit\'')
 #elif sum >= 17 and sum < 21, advise to "Stay"
+elif 17 <= sum_three < 21:
+    print(f'{sum_three}, I advise you to \'Stay\'')
 #elif sum == 21, advise "Blackjack!"
+elif sum_three == 21:
+    print(f'{sum_three} equals \'Blackjack!\'')
 #elif sum > 21, advise "Already Busted"
+else:
+    print(f'{sum_three} equals \'Already Busted\'')
 #Print out the current total point value and the advice
