b9d935e76a25a7c14095bd6bf6bee30163e74423
Matt <bmwoodside@live.com>
2020-03-03 11:51:23 -0800

    update to hangman group lab

--- code/matt/hangman_group.py
+++ code/matt/hangman_group.py
@@ -11,30 +11,36 @@ counter = 10
 for word in lines:
     if len(word) > 5:
         words.append(word)
-
-winning = list(random.choice(words))
+winning_word = random.choice(words)
+winning = list(winning_word)
 
 for letter in winning:
     blanks.append("_ ")
 
 while blanks != winning:
     print(blanks)
-    print(winning)
-    user_in = input("What letter do you guess?: ")
     print(f"Guessed: {guesses}")
-    print(f"You have {counter} guesses remaining.")
+    user_in = input("What letter do you guess?: ")
     if user_in in guesses:
         print("You've already guessed this letter!")
+        print(f"You have {counter} guesses remaining.")
     elif user_in in winning:
         for i, char in enumerate(winning):
             if user_in == char:
                 blanks[i] = char
+        guesses.append(user_in)
         print("That's in the word!")
         print(f"You have {counter} guesses remaining.")
     elif user_in not in winning:
         counter -= 1
         guesses.append(user_in)
         print(f"{user_in} not in the word!")
+        print(f"You have {counter} guesses remaining.")
+    elif counter == 0:
+        print("You ran out of tries!")
+        print(f"The winning word was {winning_word}")
     else:
         print("something went wrong")
+else:
+    print(f"You win! The word was {winning_word} and you had {counter} guesses remaining!")
 
