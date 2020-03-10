81967a020a8f1eec26a2f8894f48831affe57836
Justin Nizza <pizzaboynizza@Richards-iPhone.localdomain>
2020-03-10 16:18:48 -0700

    stupid sexy flanders

---
+++ code/Justin/pythoncapstone.py
@@ -0,0 +1,51 @@
+class Musical_Artist:
+    def __init__(self, name, popularity, influence, musical_impact, cultural_impact): 
+        self.name = name
+        self.popularity= popularity
+        self.influence = influence
+        self.musical_impact = musical_impact
+        self.cultural_impact = cultural_impact
+        # return((popularity + influence + musical_impact + cultural_impact)/4)
+
+b1 = Musical_Artist("The Beatles", 100
+b2 = Musical_Artist("Elvis Presley", 
+b3 = Musical_Artist("Bob Dylan"
+b4 = Musical_Artist("The Rolling Stones", 96)
+b5 = Musical_Artist("Stevie Wonder"
+b6 = Musical_Artist("Chuck Berry"
+b7 = Musical_Artist("The Who",
+b8 = Musical_Artist("Led Zeppelin", 99)
+b9 = Musical_Artist("Ray Charles"
+b10 = Musical_Artist("The Beach Boys"
+b11 = Musical_Artist("Aretha Franklin"
+b12 = Musical_Artist("Fats Domino"
+b13 = Musical_Artist("Michael Jackson"
+b14 = Musical_Artist("Madonna"
+b15 = Musical_Artist("Jimi Hendrix"
+b16 = Musical_Artist("Little Richard"
+b17 = Musical_Artist("Bruce Springsteen"
+b18 = Musical_Artist("Marvin Gaye"
+b19 = Musical_Artist("Sam Cooke"
+b20 = Musical_Artist("James Brown"
+b21 = Musical_Artist("Santana", 81
+b22 = Musical_Artist("CCR"), 82
+b23 = Musical_Artist("Journey", 83)
+b24 = Musical_Artist("Van Halen", 84)
+b25 = Musical_Artist("U2", 85)
+b26 = Musical_Artist("Bon Jovi", 86)
+b27 = Musical_Artist("Maroon 5", 87)
+b28 = Musical_Artist("Red Hot Chili Peppers", 88)
+b29 = Musical_Artist("Linkin Park", 89)
+b30 = Musical_Artist("Fleetwood Mac", 90)
+b31 = Musical_Artist("Aerosmith", 91)
+b32 = Musical_Artist("Guns N' Roses", 92)
+b33 = Musical_Artist("Metallica", 93)
+b34 = Musical_Artist("The Eagles", 94)
+b35 = Musical_Artist("AC/DC", 95)
+b36 = Musical_Artist("Pink Floyd", 97)
+b37 = Musical_Artist("Queen", 98)
+
+
+
+print(b1.popularity)
+
