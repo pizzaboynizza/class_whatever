0d060ce35b87913ccd2d3b6cd24217ed2b357e9d
TinaShreves <tina.l.shreves@gmail.com>
2020-03-10 10:59:30 -0700

    chatbot

--- code/tina/bot/test.py
+++ code/tina/bot/test.py
@@ -1,11 +1,4 @@
-# class Slapper(commands.Converter):
-#     async def convert(self, ctx,argument):
-#         to_slap = random.choice(ctx.guild.members)
-#         return (f'{3} slapped {1} because *{2}*',(ctx, to_slap, argument))
 
-# @bot.command()
-# async def slap1(ctx, * , reason:Slapper()):
-#     await ctx.send(reason)
 
 
 @client.event
