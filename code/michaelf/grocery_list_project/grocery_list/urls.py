d66991018f178928327e71801348a794c74b5078
Michael Finn <mikeysight@gmail.com>
2020-04-02 15:11:35 -0700

    finished groc list

--- code/michaelf/grocery_list_project/grocery_list/urls.py
+++ code/michaelf/grocery_list_project/grocery_list/urls.py
@@ -6,4 +6,6 @@ app_name='grocery_list'
 urlpatterns=[
     path('', views.index, name='index'),
     path('add/', views.add, name='add'),
+    path('update/', views.update, name='update'),
+    path('delete/', views.delete, name='delete'),
 ]
--- code/michaelf/grocery_list_project/grocery_list/views.py
+++ code/michaelf/grocery_list_project/grocery_list/views.py
@@ -15,3 +15,33 @@ def add(request):
     add_item=GroceryItem(text=request.POST.get('item'))
     add_item.save()
     return HttpResponseRedirect(reverse('grocery_list:index'))
+
+def update(request):
+    for item in request.POST:
+        if item.isdigit()==True:
+            completed_item= GroceryItem.objects.get(pk=item)
+            completed_item.date_completed=timezone.now()
+            completed_item.save()
+            print(completed_item)
+        else:
+            pass
+        # completed_item= GroceryItem.objects.get(pk=item)
+        # print(completed_item)
+    
+
+    # completed_items=GroceryItem.objects.filter(request.POST["completed"])
+    # completed_items = GroceryItem.objects.filter(date_completed__isnull=False)
+    # completed_items.delete()
+
+    # return HttpResponse("Test")
+
+    return HttpResponseRedirect(reverse('grocery_list:index'))
+
+def delete(request):
+    for item in GroceryItem.objects.all():
+        print(item)
+        if item.is_completed():
+            item.delete()
+        else:
+            pass
+    return HttpResponseRedirect(reverse('grocery_list:index'))
