2a472638d205a48577a0c57ad838c1f473a0adb1
JoeD42 <joepdean@gmail.com>
2020-04-02 12:00:09 -0700

    django lab1

--- code/joe/Django/lab1/grocery_list/models.py
+++ code/joe/Django/lab1/grocery_list/models.py
@@ -4,7 +4,7 @@ class GroceryItem(models.Model):
     text_description = models.CharField(max_length = 200)
     date_created = models.DateTimeField("date created")
     date_completed = models.DateTimeField("date completed", null=True, blank=True)
-    is_completed = models.BooleanField()
+    is_completed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.text_description
