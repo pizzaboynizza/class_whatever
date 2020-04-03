4368fce93e0125203e038043457992e7bd529ba0
JoHannah Joy <jojojoy2012@gmail.com>
2020-04-03 10:29:28 -0700

    rename folders and edit settings and other files to rename folders. begin setup of database

--- code/johannah/grocery_list_lab_1/grocery_list_app/views.py
+++ code/johannah/grocery_list_lab_1/grocery_list_app/views.py
@@ -1,7 +1,12 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
 def index(request):
-  return HttpResponse('<h1>Programming Database</h1>')
+  template = loader.get_template('grocery_list/index.html')
+  context = {
+    'message': 'Hello, world.'
+  }
+  return HttpResponse(template.render(context, request))
--- code/johannah/grocery_list_lab_1/grocery_list_project/wsgi.py
+++ code/johannah/grocery_list_lab_1/grocery_list_project/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grocery_list_lab_1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grocery_list_project.settings')
 
 application = get_wsgi_application()
