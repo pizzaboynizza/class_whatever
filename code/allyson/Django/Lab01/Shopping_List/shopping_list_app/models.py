742754563482d69e9da5dfc04e27c6a26d04f185
Allyson <Cl0udF1sh@hotmail.com>
2020-04-06 10:04:01 -0700

    FIXED!

--- code/allyson/Django/Lab01/Shopping_List/shopping_list_app/models.py
+++ code/allyson/Django/Lab01/Shopping_List/shopping_list_app/models.py
@@ -3,11 +3,11 @@ from django.db import models
 from django.utils import timezone
 
 
-class Items_List(models.Models):
+class Items_List(models.Model):
     item = models.CharField(max_length=50)
     add_date = models.DateTimeField("date created")
     com_date = models.DateTimeField(null=True, blank=True)
-    completed=models.BooleanField(null=True, Blank=True)
+    completed=models.BooleanField(null=True, blank=True)
 
     def __str__(self):
         return self.item + ""+"("+str(self.completed)+")"
